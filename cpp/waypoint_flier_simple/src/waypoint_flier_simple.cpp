73f19cf64cdc7ac485c2ab2a83d41a451e98d993
amit-420 <amitsawant2424@gmail.com>
2025-07-23 18:42:21 +0200

    waypoint_flier simple initial commit

--- cpp/waypoint_flier_simple/src/waypoint_flier_simple.cpp
+++ cpp/waypoint_flier_simple/src/waypoint_flier_simple.cpp
@@ -1,238 +1,226 @@
 /* includes //{ */
 
-/* each ros package must have these */
-#include <ros/ros.h>
-#include <ros/package.h>
-#include <nodelet/nodelet.h>
+/*each ros package must have these*/
+#include <rclcpp/rclcpp.hpp>
 
 /* for storing information about the state of the uav (position, twist) + covariances */
-#include <nav_msgs/Odometry.h>
+#include <nav_msgs/msg/odometry.hpp>
 
 /* custom msgs of MRS group */
-#include <mrs_msgs/ReferenceStamped.h>
+#include <mrs_msgs/msg/reference_stamped.hpp>
 
 /* for calling simple ros services */
-#include <std_srvs/Trigger.h>
+#include <std_srvs/srv/trigger.hpp>
+
+#include <waypoint_flier_simple/params.h>
 
 #include <random>
+
+using namespace std::chrono_literals;
+
 //}
 
+
+/* what is subnode for paramter loading*/
 namespace example_waypoint_flier_simple
 {
+    class WaypointFlierSimple : public rclcpp::Node
+    {
+    public:
+        WaypointFlierSimple(const rclcpp::NodeOptions& options);
+        void intialize();
+    private:
+        rclcpp::Node::SharedPtr node_;
+        rclcpp::Clock::SharedPtr clock_;
 
-/* class WaypointFlierSimple //{ */
+        /* ros paramters*/
+        // | -------------------------- flags ------------------------- |
 
-class WaypointFlierSimple : public nodelet::Nodelet {
+        /* is set to true when the nodelet is initialized, useful for rejecting callbacks that are called before the node is initialized */
+        std::atomic<bool> is_initialized_ = false;
+        bool loaded_successfully = true;
 
-public:
-  /* onInit() is called when nodelet is launched (similar to main() in regular node) */
-  virtual void onInit();
+        /* by default, the node component is deactivated, it only starts publishing goals when activated */
+        std::atomic<bool> active_ = false;
 
-private:
-  // | -------------------------- flags ------------------------- |
+        /* by default, the nodel component is deactivated, it only starts publishing goals when activated */
+        std::atomic<bool> have_odom_ = false;
 
-  /* is set to true when the nodelet is initialized, useful for rejecting callbacks that are called before the node is initialized */
-  std::atomic<bool> is_initialized_ = false;
+        /* variables to store the coordinates of the current goal */
+        double goal_x_ = 0.0;
+        double goal_y_ = 0.0;
+        double goal_z_ = 2.0;
 
-  /* by default, the nodelet is deactivated, it only starts publishing goals when activated */
-  std::atomic<bool> active_ = false;
+        /* variables to store the maximum limit for the random waypoint generator */
+        double max_x_;
+        double max_y_;
+        double max_z_;
 
-  /* by default, the nodelet is deactivated, it only starts publishing goals when activated */
-  std::atomic<bool> have_odom_ = false;
+        /* ROS messages which store the current reference and odometry */
+        mrs_msgs::msg::ReferenceStamped ref_;
+        nav_msgs::msg::Odometry         current_odom_;
 
-  /* variables to store the coordinates of the current goal */
-  double goal_x_ = 0.0;
-  double goal_y_ = 0.0;
-  double goal_z_ = 2.0;
+        // | ---------------------- ROS subscribers --------------------- |
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom_;
+        void            callbackOdom(const nav_msgs::msg::Odometry& msg);
 
-  /* variables to store the maximum limit for the random waypoint generator */
-  double max_x_;
-  double max_y_;
-  double max_z_;
+        // | ---------------------- ROS publishers --------------------- |
+        void callbackMainTimer();
+        rclcpp::TimerBase::SharedPtr    timer_publisher_reference_;
+        rclcpp::Publisher<mrs_msgs::msg::ReferenceStamped>::SharedPtr publisher_reference_;
 
-  /* ROS messages which store the current reference and odometry */
-  mrs_msgs::ReferenceStamped ref_;
-  nav_msgs::Odometry         current_odom_;
+        // | ---------------------- ROS timers --------------------- |
 
-  // | ---------------------- ROS subscribers --------------------- |
-  ros::Subscriber sub_odom_;
-  void            callbackOdom(const nav_msgs::Odometry& msg);
+        rclcpp::TimerBase::SharedPtr timer_initializer_;
+        void      initialize();
 
-  // | ---------------------- ROS publishers --------------------- |
-  ros::Publisher pub_reference_;
+        // | ---------------------- ROS service servers --------------------- |
 
-  // | ---------------------- ROS timers --------------------- |
+        bool               callbackStart([[maybe_unused]] const std::shared_ptr<std_srvs::srv::Trigger::Request> req, const std::shared_ptr<std_srvs::srv::Trigger::Response> res);
+        rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr srv_server_start_;
 
-  ros::Timer main_timer_;
-  void       callbackMainTimer([[maybe_unused]] const ros::TimerEvent& te);
+        // | ------------------ Additional functions ------------------ |
 
-  // | ---------------------- ROS service servers --------------------- |
+        double distance(const mrs_msgs::msg::ReferenceStamped& waypoint, const nav_msgs::msg::Odometry& odom);
 
-  bool               callbackStart([[maybe_unused]] std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res);
-  ros::ServiceServer srv_server_start_;
+        double getRandomDouble(double min, double max);
+        
+    };
 
-  // | ------------------ Additional functions ------------------ |
-
-  double distance(const mrs_msgs::ReferenceStamped& waypoint, const nav_msgs::Odometry& odom);
-
-  double getRandomDouble(double min, double max);
-};
-//}
-
-/* onInit() //{ */
-
-void WaypointFlierSimple::onInit() {
-
-  /* obtain node handle */
-  ros::NodeHandle nh = nodelet::Nodelet::getMTPrivateNodeHandle();
-
-  /* initialize random number generator */
-  srand(time(NULL));
-
-  /* waits for the ROS to publish clock */
-  ros::Time::waitForValid();
-
-  // | ------------------- load ros parameters ------------------ |
-  nh.param("max_x", max_x_, 10.0);
-  nh.param("max_y", max_y_, 10.0);
-  nh.param("max_z", max_z_, 5.0);
-
-  // | -------- initialize a subscriber for UAV odometry -------- |
-  sub_odom_ = nh.subscribe("odom_in", 10, &WaypointFlierSimple::callbackOdom, this, ros::TransportHints().tcpNoDelay());
-
-  // | -------- initialize a publisher for UAV reference -------- |
-  pub_reference_ = nh.advertise<mrs_msgs::ReferenceStamped>("reference_out", 1);
-
-  // | -- initialize the main timer - main loop of the nodelet -- |
-  main_timer_ = nh.createTimer(ros::Rate(10), &WaypointFlierSimple::callbackMainTimer, this);
-
-  // | ---------------- initialize service server --------------- |
-  srv_server_start_ = nh.advertiseService("start", &WaypointFlierSimple::callbackStart, this);
-  is_initialized_   = true;
-}
-
-//}
-
-// | ---------------------- msg callbacks --------------------- |
-
-/* callbackOdom() //{ */
-
-
-void WaypointFlierSimple::callbackOdom(const nav_msgs::Odometry& msg) {
-
-  /* do not continue if the nodelet is not initialized */
-  if (!is_initialized_) {
-    return;
-  }
-  // | -------------- save the current UAV odometry ------------- |
-  current_odom_ = msg;
-  have_odom_    = true;
-}
-
-//}
-
-// | --------------------- timer callbacks -------------------- |
-
-/* callbackMainTimer() //{ */
-
-void WaypointFlierSimple::callbackMainTimer([[maybe_unused]] const ros::TimerEvent& te) {
-
-  if (!active_) {
-
-    ROS_INFO_THROTTLE(1.0, "[WaypointFlierSimple]: Waiting  for activation");
-
-  } else {
-
-    // calculate the distance to the current reference
-    double dist_to_ref = distance(ref_, current_odom_);
-    ROS_INFO_STREAM_THROTTLE(1.0, "[WaypointFlierSimple]: Distance to reference: " << dist_to_ref);
-
-    // if the distance is less than 1 meter, generate a new reference
-    if (dist_to_ref < 1.0) {
-
-      ROS_INFO_STREAM("[WaypointFlierSimple]: Goal reached!");
-
-      // generate new goal
-      goal_x_ = getRandomDouble(-max_x_, max_x_);
-      goal_y_ = getRandomDouble(-max_y_, max_y_);
-      goal_z_ = getRandomDouble(2.0, max_z_);
-
-      ROS_INFO_STREAM("[WaypointFlierSimple]: New goal X: " << goal_x_ << " Y: " << goal_y_ << " Z: " << goal_z_);
+    WaypointFlierSimple::WaypointFlierSimple(const rclcpp::NodeOptions& options): Node("example_waypoint_flier_simple", options)
+    {
+        timer_initializer_ = create_wall_timer(std::chrono::duration<double>(1.0), std::bind(&WaypointFlierSimple::initialize,this));
     }
 
-    // fill out the ROS message and publish it
-    ref_.reference.position.x = goal_x_;
-    ref_.reference.position.y = goal_y_;
-    ref_.reference.position.z = goal_z_;
-    ref_.reference.heading    = 0.0;
-    pub_reference_.publish(ref_);
-  }
-}
+    void WaypointFlierSimple::initialize(){
+        
+        /*------------ load parameters --------- */
+        loaded_successfully &= utils::load_param<double>("max_x", max_x_, 10.0, *node_);
+        loaded_successfully &= utils::load_param("max_y", max_y_, 10.0, *node_);
+        loaded_successfully &= utils::load_param("max_z", max_z_, 5.0, *node_);
+        
+        if (!loaded_successfully)
+        {
+            RCLCPP_INFO_ONCE(node_->get_logger(),"Failed to load non optional parameters");
+        }        
 
-//}
+        // | -------- initialize a publisher for UAV reference -------- |
+        publisher_reference_ = node_->create_publisher<mrs_msgs::msg::ReferenceStamped>("reference_out", 1);
+        timer_publisher_reference_ = create_wall_timer(std::chrono::duration<double>(1.0), std::bind(&WaypointFlierSimple::callbackMainTimer, this));
 
-// | -------------------- service callbacks ------------------- |
 
-/* //{ callbackStart() */
+        // |--------------- service server for starting waypoint following ---------------|
+        srv_server_start_ = create_service<std_srvs::srv::Trigger>("~/start_waypoint_flying", std::bind(&WaypointFlierSimple::callbackStart, this, std::placeholders::_1, std::placeholders::_2));
 
-bool WaypointFlierSimple::callbackStart([[maybe_unused]] std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res) {
+        is_initialized_ = true;
 
-  // | ------------------- activation service ------------------- |
-  // only activates the main loop when the nodelet is initialized and receiving odometry
+        timer_initializer_->cancel();
 
-  if (!is_initialized_) {
 
-    res.success = false;
-    res.message = "Waypoint flier not initialized!";
-    ROS_WARN("[WaypointFlierSimple]: Cannot start waypoint following, nodelet is not initialized.");
-    return true;
-  }
 
-  if (!have_odom_) {
+    }
 
-    res.success = false;
-    res.message = "Waypoint flier is not receiving odometry!";
-    ROS_WARN("[WaypointFlierSimple]: Cannot start, nodelet is not receiving odometry!");
-    return true;
-  }
+    // |------------------ msg_callbacks ------------------------|
+    /* callbackodom() //{ */
+    void WaypointFlierSimple::callbackOdom(const nav_msgs::msg::Odometry& msg){
+        /* do not continue if nodelet is not initialized*/
+        if (!is_initialized_)
+        {
+            return;
+        }
+        // | -------------- save the current UAV odometry ------------- |
+        current_odom_ = msg;
+        have_odom_    = true;
+        
+    }
+    /* }*/
 
-  active_ = true;
+    // |------------------ timer callbacks ---------------------------|
+    /* callbackMainTimer() //{ */
+    void WaypointFlierSimple::callbackMainTimer(){
+        if (!active_)
+        {
+            RCLCPP_INFO(node_->get_logger(), "[ExampleWaypointFlierSimple]: waypoint flier is not activated yet."); 
+        }
+        
+        const double curr_dist = distance(ref_, current_odom_);
 
-  ROS_INFO("[WaypointFlierSimple]: Starting waypoint following.");
-  ROS_INFO_STREAM("[WaypointFlierSimple]: Goal X: " << goal_x_ << " Y: " << goal_y_ << " Z: " << goal_z_);
+        if(curr_dist < 1.0){
+            RCLCPP_INFO_STREAM(node_->get_logger(), "[WaypointFlierSimple]: Goal reached!");
+            /* select new reference point */
+            goal_x_ = getRandomDouble(-max_x_,max_x_);
+            goal_y_ = getRandomDouble(-max_y_,max_y_);
+            goal_z_ = getRandomDouble(-max_z_,max_z_);
 
-  res.success = true;
-  res.message = "Starting waypoint following.";
+            RCLCPP_INFO_STREAM(node_->get_logger(), "[WaypointFlierSimple]: New goal X: " << goal_x_ << " Y: " << goal_y_ << " Z: " << goal_z_);
+        }
 
-  return true;
-}
+        ref_.reference.position.x = goal_x_;
+        ref_.reference.position.y = goal_y_;
+        ref_.reference.position.z = goal_z_;  
+        ref_.reference.heading = 0.0;
+        
+        publisher_reference_->publish(ref_);
+        
+        
+    }
+    /* }*/
 
-//}
+    bool WaypointFlierSimple::callbackStart([[maybe_unused]] const std::shared_ptr<std_srvs::srv::Trigger::Request> req, const std::shared_ptr<std_srvs::srv::Trigger::Response> res){
 
-// | -------------------- support functions ------------------- |
+        // | ------------------- activation service ------------------- |
+        // only activates the main loop when the nodelet is initialized and receiving odometry
 
-/* distance() //{ */
+        if (!is_initialized_) {
 
-double WaypointFlierSimple::distance(const mrs_msgs::ReferenceStamped& waypoint, const nav_msgs::Odometry& odom) {
+            res->success = false;
+            res->message = "Waypoint flier not initialized!";
+            RCLCPP_WARN(node_->get_logger(), "[WaypointFlierSimple]: Cannot start waypoint following, nodelet is not initialized.");
+            return true;
+        }
 
-  // | ------------- distance between two 3D points ------------- |
+        if (!have_odom_) {
 
-  return sqrt((pow(waypoint.reference.position.x - odom.pose.pose.position.x, 2)) + (pow(waypoint.reference.position.y - odom.pose.pose.position.y, 2)) +
-              (pow(waypoint.reference.position.z - odom.pose.pose.position.z, 2)));
-}
+            res->success = false;
+            res->message = "Waypoint flier is not receiving odometry!";
+            RCLCPP_WARN(node_->get_logger(), "[WaypointFlierSimple]: Cannot start, nodelet is not receiving odometry!");
+            return true;
+        }
 
-double WaypointFlierSimple::getRandomDouble(double min, double max) {
+        active_ = true;
 
-  // | --------- random double in the min and max bounds -------- |
+        RCLCPP_INFO(node_->get_logger(), "[WaypointFlierSimple]: Starting waypoint following.");
+        RCLCPP_INFO_STREAM(node_->get_logger(), "[WaypointFlierSimple]: Goal X: " << goal_x_ << " Y: " << goal_y_ << " Z: " << goal_z_);
 
-  float r = (float)rand() / (float)RAND_MAX;
-  return min + r * (max - min);
-}
+        res->success = true;
+        res->message = "Starting waypoint following.";
 
-//}
+        return true;
 
-}  // namespace example_waypoint_flier_simple
 
-/* every nodelet must include macros which export the class as a nodelet plugin */
-#include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(example_waypoint_flier_simple::WaypointFlierSimple, nodelet::Nodelet);
+    }
+
+    // | -------------------- support functions ------------------- |
+
+    /* distance() //{ */
+
+    double WaypointFlierSimple::distance(const mrs_msgs::msg::ReferenceStamped& waypoint, const nav_msgs::msg::Odometry& odom) {
+
+        // | ------------- distance between two 3D points ------------- |
+
+        return sqrt((pow(waypoint.reference.position.x - odom.pose.pose.position.x, 2)) + (pow(waypoint.reference.position.y - odom.pose.pose.position.y, 2)) +
+                    (pow(waypoint.reference.position.z - odom.pose.pose.position.z, 2)));
+        }
+
+    double WaypointFlierSimple::getRandomDouble(double min, double max) {
+
+        // | --------- random double in the min and max bounds -------- |
+
+        float r = (float)rand() / (float)RAND_MAX;
+        return min + r * (max - min);
+    }
+    
+} // end of example_waypoint_flier_simple namespace
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(example_waypoint_flier_simple::WaypointFlierSimple)
