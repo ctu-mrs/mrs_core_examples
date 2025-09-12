6791be8f380211f35e523bf9c74ac8f693d865c3
amit-420 <amitsawant2424@gmail.com>
2025-09-12 14:06:56 +0200

    estimator example: compilation and runtime errors fixed

--- cpp/example_estimator_plugin/tmux/start.sh
+++ cpp/example_estimator_plugin/tmux/start.sh
@@ -10,7 +10,7 @@ export TMUX_SESSION_NAME=simulation
 export TMUX_SOCKET_NAME=mrs
 
 # start tmuxinator
-tmuxinator start -p ./session.yml
+tmuxinator start -p ./session.yaml
 
 # if we are not in tmux
 if [ -z $TMUX ]; then
