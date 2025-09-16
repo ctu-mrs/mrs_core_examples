28b996f042bc3e11e288ef52d84c50d5d852eb22
amit-420 <amitsawant2424@gmail.com>
2025-09-16 21:52:37 +0200

    example estimator plugin:  runtime issues fixed

--- cpp/example_estimator_plugin/src/example_estimator_plugin.cpp
+++ cpp/example_estimator_plugin/src/example_estimator_plugin.cpp
@@ -16,34 +16,16 @@ namespace mrs_uav_state_estimators
 
 namespace example_estimator_plugin
 {
-
  
 const char estimator_name[] = "example_estimator_plugin";
 const bool is_core_plugin   = false;
 
-/* package name is defined as a namespace property with default value of 'mrs_uav_state_estimators'. We have to override it.*/
-// namespace state_generic
-// {
-//    char package_name[] = "example_estimator_plugin";
-// }
-
 class ExampleEstimator : public mrs_uav_state_estimators::StateGeneric
 {
 public:
-    ExampleEstimator():StateGeneric(estimator_name, is_core_plugin){}
+    ExampleEstimator():StateGeneric(estimator_name, is_core_plugin){}  
 
-    // | --------------- param loader description ----------------- |
-    // ph_->param_loader->addYamlFile(ament_index_cpp::get_package_share_directory(package_name_) + "/config/private/" + getName() + ".yaml");
-    // ph_->param_loader->addYamlFile(ament_index_cpp::get_package_share_directory(package_name_) + "/config/public/" + getName() + ".yaml");
-  
-
-    // | --------------- subscriber initialization ---------------- |
     
-
-    // | ---------------- publishers initialization --------------- |
-
-    // ph_odom_ = mrs_lib::PublisherHandler<nav_msgs::msg::Odometry>(this, "~/" + Support::toSnakeCase(getName()) + "/odom");
-
 };
 } // namespace example_estimator plugin
 } // namespace mrs_uav_state_estimators
