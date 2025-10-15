e7a202ecde72dfef95f256aab4673eaeabdfd73e
Tomas Baca <Tomas Baca>
2025-10-15 10:12:03 +0200

    polished the example estimator plugin

--- cpp/example_estimator_plugin/src/example_estimator_plugin.cpp
+++ cpp/example_estimator_plugin/src/example_estimator_plugin.cpp
@@ -1,36 +1,22 @@
 #include <rclcpp/rclcpp.hpp>
 
-#include <mrs_lib/param_loader.h>
-#include <mrs_lib/lkf.h>
-#include <mrs_lib/repredictor.h>
-#include <mrs_lib/profiler.h>
-#include <mrs_lib/subscriber_handler.h>
-#include <mrs_lib/dynparam_mgr.h>
-
 #include <mrs_uav_managers/state_estimator.h>
 #include <mrs_uav_state_estimators/estimators/state/state_generic.h>
 
-
-namespace mrs_uav_state_estimators
-{
-
 namespace example_estimator_plugin
 {
- 
+
 const char estimator_name[] = "example_estimator_plugin";
 const bool is_core_plugin   = false;
 
-class ExampleEstimator : public mrs_uav_state_estimators::StateGeneric
-{
+class ExampleEstimator : public mrs_uav_state_estimators::StateGeneric {
 public:
-    ExampleEstimator():StateGeneric(estimator_name, is_core_plugin){}  
-
-    
+  ExampleEstimator() : StateGeneric(estimator_name, is_core_plugin) {
+  }
 };
-} // namespace example_estimator plugin
-} // namespace mrs_uav_state_estimators
+
+}  // namespace example_estimator_plugin
 
 
 #include <pluginlib/class_list_macros.hpp>
-
-PLUGINLIB_EXPORT_CLASS(mrs_uav_state_estimators::example_estimator_plugin::ExampleEstimator, mrs_uav_managers::StateEstimator)
+PLUGINLIB_EXPORT_CLASS(example_estimator_plugin::ExampleEstimator, mrs_uav_managers::StateEstimator)
