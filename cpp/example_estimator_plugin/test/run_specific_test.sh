d1f6bb86d8dddf08a4df88f24abebdb797c35abf
Tomas Baca <Tomas Baca>
2025-10-16 15:21:23 +0200

    estimator plugin: added integration test

---
+++ cpp/example_estimator_plugin/test/run_specific_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while [ ! -e "build/COLCON_IGNORE" ]; do
+  cd ..
+  if [[ `pwd` == "/" ]]; then
+    # we reached the root and didn't find the build/COLCON_IGNORE file - that's a fail!
+    echo "Cannot compile, probably not in a workspace (if you want to create a new workspace, call \"colcon init\" in its root first)".
+    exit 1
+  fi
+done
+
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+
+colcon test-result --delete-yes
+
+colcon test --packages-select example_estimator_plugin --ctest-args -R 'goto_service' --event-handlers console_direct+ console_stderr- console_start_end-
+
+colcon test-result --all --verbose
