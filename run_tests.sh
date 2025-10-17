4d061b3691566965de14449581ba27646c807579
Tomas Baca <Tomas Baca>
2025-10-17 10:54:28 +0200

    added script for testing all packages at once

---
+++ run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MY_PATH=`pwd`
+
+while [ ! -e "build/COLCON_IGNORE" ]; do
+  cd ..
+  if [[ `pwd` == "/" ]]; then
+    # we reached the root and didn't find the build/COLCON_IGNORE file - that's a fail!
+    echo "Cannot compile, probably not in a workspace (if you want to create a new workspace, call \"colcon init\" in its root first)".
+    exit 1
+  fi
+done
+
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+
+colcon test-result --delete-yes
+
+pkgs=$(colcon list -n)
+
+colcon test --executor sequential --ctest-args --packages-select $pkgs
+
+colcon test-result --all --verbose
